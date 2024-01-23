4f81810b955a27d0abe59d81f8a5bb36bf98037b
duykhue257 <nguyenduykhue257@gmail.com>
2024-01-23 17:03:59 +0700

    feat_writing

---
+++ src/pages/writing/index.tsx
@@ -0,0 +1,178 @@
+const Writing = () => {
+    return (
+      <div>
+        <div className='grid grid-cols-6 gap-4 overflow-y-auto md:pt-4 pt-4 px-6 h-full w-full'>
+          <div className='col-span-full lg:col-span-4 mt-[100px]'>
+            <p className='mb-3 text-[20px]'>It is worth noting that there is an increasing amount of waste being produced in our societies today, resulting in pollution and environmental degredation. This phenominon is largly owing to people's uncontrolled consumption patterns and untidy behaviours. The government should take actions to tackle this issue in order to maintain a clean environment.</p>
+            <p className="text-[20px]">
+              To begin with, people are producing more rubbish because they consume more goods. For instance, the improvement of living standards over the years has equipped residents with higher purchasing power, resulting in many shopping for items they don't necessarily need. Consequently, more rubbish are produced by customers. Furthermore, some people do not adhere to proper waste disposal methods, leading to littering in public spaces such as streets, rivers and parks. These two factors predominatly contribute to more rubbish production.
+            </p>
+            <p className='my-3 text-[20px]'>
+              There are two effective solutions to the problem of waste. One way to tackle this issue is providing education to the public and informing them about the importance of sustainable way of living and environmental cleaniness. One excellent example of this involves community campaigns, through which residents are able to gain a deeper understanding of the scarcity of natural resources and the consequences of exploiting them. In addition, the government can also impose waste management regulations which can correct littering behaviours through heavy fines and imprisonment. Once residents absorb the serious implications of littering, they will in turn reduce and stop throwing rubbish around.
+            </p>
+            <p className="text-[20px]">
+              In conclusion, the problem of more rubbish production nowadays can be attributed to unsustainable consumption and unregulated behaviours. As a result, the government should promote environmental sustainability education as well as enforce laws to combat the issue of waste.
+            </p>
+  
+            <div className='flex my-6 '>
+              <div className='border-none bg-yellow-300 w-[10px]'></div>
+              <div className='border border-gray-300  shadow-lg p-2  '>
+                <p className='text-[20px] font-medium mb-2'>Task Response</p>
+                <p>Ensure all parts of the task are fully addressed, which includes discussing not just why more rubbish is produced but also providing in-depth explanations of government intervention methods.</p>
+              </div>
+            </div>
+            <div className='flex my-6'>
+              <div className='border-none bg-yellow-300 w-[10px]'></div>
+              <div className='border border-gray-300  shadow-lg p-2  '>
+                <p className='text-[20px] font-medium mb-2'>Task Response</p>
+                <p>Ensure all parts of the task are fully addressed, which includes discussing not just why more rubbish is produced but also providing in-depth explanations of government intervention methods.</p>
+              </div>
+            </div>
+            <div className='flex my-6'>
+              <div className='border-none bg-yellow-300 w-[10px]'></div>
+              <div className='border border-gray-300  shadow-lg p-2  '>
+                <p className='text-[20px] font-medium mb-2'>Task Response</p>
+                <p>Ensure all parts of the task are fully addressed, which includes discussing not just why more rubbish is produced but also providing in-depth explanations of government intervention methods.</p>
+              </div>
+            </div>
+            <div className='flex my-6'>
+              <div className='border-none bg-yellow-300 w-[10px]'></div>
+              <div className='border border-gray-300  shadow-lg p-2  '>
+                <p className='text-[20px] font-medium mb-2'>Task Response</p>
+                <p>Ensure all parts of the task are fully addressed, which includes discussing not just why more rubbish is produced but also providing in-depth explanations of government intervention methods.</p>
+              </div>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className=' col-span-full lg:col-span-2'>
+            <div className='border-none rounded-xl p-3 bg-green-200 flex justify-content-around  shadow'>
+              <div className='border-none rounded-full bg-white w-[40px] h-[40px] flex items-center justify-center font-bold text-red-400'>10</div>
+              <div>
+                <p className='font-bold'>Linking words</p>
+                <p>meeting the goal of 7 or more</p>
+              </div>
+            </div>
+            <div className='border-none my-3 rounded-xl p-3 bg-yellow-100 flex justify-content-around border-b-2 shadow'>
+              <div className='border-none rounded-full bg-white w-[40px] h-[40px] flex items-center justify-center font-bold text-red-400'>2</div>
+              <div>
+                <p className='font-bold'>Word repetition</p>
+                <p>meeting the goal of 3 or fewer</p>
+              </div>
+            </div><div className='border-none rounded-xl p-3 bg-red-100 flex justify-content-around border-b-2 shadow'>
+              <div className='border-none rounded-full bg-white w-[40px] h-[40px] flex items-center justify-center font-bold text-red-400'>8</div>
+              <div>
+                <p className='font-bold'>Grammar mistakes</p>
+                <p>meeting the goal of 3 or fewer</p>
+              </div>
+            </div>
+              <div className='flex my-3 justify-content-around align-items-center'>
+                    <p><span className='text-green-500 mr-2'>4</span>paragraphs</p>
+                    <p><span  className='text-green-500 mr-2'>275 </span>words</p>
+              </div>
+              <div className='border-dashed border-1 my-[50px]'></div>
+            <div className=" px-2">
+              <p className="text-[96px] text-center font-bold text-green-400 my-4 ">5.5</p>
+              <p className="text-[36px] font-normal text-center mb-[90px]">Overall Band Score</p>
+              <div className='border-dashed border-1 my-[50px]'></div>
+              <div>
+                <p className="font-bold text-[24px] mb-2">Coherence and cohesion: 5.0</p>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">7</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Introduction & conclusion present</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">8</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Supported main points</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-red-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">6</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Accurate linking words</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">9</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Variety in linking words</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">9</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Logical structure</p>
+                </div>
+              </div>
+              <div className='border-dashed border-1 my-[50px]'></div>
+              <div>
+                <p className="font-bold text-[24px] mb-2">LEXICAL RESOURCE: 4.0</p>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">6</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Logical structure</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-red-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">6</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Logical structure</p>
+                </div>
+              </div>
+              <div className='border-dashed border-1 my-[50px]'></div>
+              <div>
+                <p className="font-bold text-[24px] mb-2">GRAMMATICAL RANGE: 4.0</p>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">9</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Mix of complex & simple sentences</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">4</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Clear and correct grammar</p>
+                </div>
+              </div>
+              <div className='border-dashed border-1 my-[50px]'></div>
+              <div>
+                <p className="font-bold text-[24px] mb-2">TASK ACHIEVEMENT: 6.0</p>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">7</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Complete response</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">7</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Clear & comprehensive ideas</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-red-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">6</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Relevant & specific examples</p>
+                </div>
+                <div className="flex gap-1 mb-2">
+                  <div className=" w-[30px] h-[30px] bg-green-500 rounded-lg">
+                    <p className=" mt-[5px] text-[15px] text-center text-white  font-bold ">6</p>
+                  </div>
+                  <p className="mt-[5px] text-[18px] ">Appropriate word count</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Writing
+  
